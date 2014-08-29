fdc374c3b93d26980cb132344039d88d3e4c2660
happyxyl <happyxyl@users.noreply.github.com>
2014-08-29 21:27:32 +0800

    JavaScript的变量作用域3-JavaScript没有块级作用域
    仔细观察下面的代码，你会发现变量i、j、k作用域是相同的，他们在整个rain函数体内都是全局的。

---
+++ var_scope_003.js
@@ -0,0 +1,15 @@
+function snow_fly(){
+    var i = 5;	
+	if (true) {
+		var j = 10;
+		for (var k = 0; k < 3; k++) {
+			console.log(k);
+		};
+		console.log(k);
+	};
+	console.log(k);
+	console.log(i);
+  console.log(j);
+}
+
+snow_fly();
